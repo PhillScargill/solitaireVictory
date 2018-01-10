3b10d3ac4f274a39595f44a276b8ac79517f77de
Jenkum <phill.scargill@gmail.com>
2018-01-10 19:18:18 +0000

    Added more tongues
    More tongues and some css tweaks

--- js/main.js
+++ js/main.js
@@ -1,9 +1,66 @@
+var interval = 10000;
+
 $(document).on('ready', function() {
-    window.setInterval(NewTongue, 3000);
+    NewTongue1();
+    NewTongue2();
+    NewTongue3();
+    NewTongue4();
+    NewTongue5();
+    window.setInterval(NewTongue1, interval);
+    window.setInterval(NewTongue2, interval);
+    window.setInterval(NewTongue3, interval);
+    window.setInterval(NewTongue4, interval);
+    window.setInterval(NewTongue5, interval);
+
 })
 
-function NewTongue(){
-    var randPosX = Math.floor((Math.random()*($(window).width()/2)));
+function NewTongue1(){
+    var randPosX = Math.floor((Math.random()*$('#tonguezone').width()));
+    $('#tongueimg')
+        .clone()
+        .css('left', randPosX)
+        .appendTo("#tonguezone")
+        .solitaireVictory();
+}
+
+function NewTongue2(){
+    var randPosX = Math.floor((Math.random()*$('#tonguezone').width()));
+    $('#tongueimg')
+        .clone()
+        .css('left', randPosX)
+        .appendTo("#tonguezone")
+        .solitaireVictory();
+}
+
+function NewTongue3(){
+    var randPosX = Math.floor((Math.random()*$('#tonguezone').width()));
+    $('#tongueimg')
+        .clone()
+        .css('left', randPosX)
+        .appendTo("#tonguezone")
+        .solitaireVictory();
+}
+
+function NewTongue3(){
+    var randPosX = Math.floor((Math.random()*$('#tonguezone').width()));
+    $('#tongueimg')
+        .clone()
+        .css('left', randPosX)
+        .appendTo("#tonguezone")
+        .solitaireVictory();
+}
+
+function NewTongue4(){
+    var randPosX = Math.floor((Math.random()*$('#tonguezone').width()));
+    $('#tongueimg')
+        .clone()
+        .css('left', randPosX)
+        .appendTo("#tonguezone")
+        .solitaireVictory();
+}
+
+function NewTongue5(){
+    var randPosX = Math.floor((Math.random()*$('#tonguezone').width()));
     $('#tongueimg')
         .clone()
         .css('left', randPosX)
